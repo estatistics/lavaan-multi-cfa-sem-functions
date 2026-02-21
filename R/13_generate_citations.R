db649e0636fb10350775fb5a75ea640134471f99
Elias Tsolis <41580704+estatistics@users.noreply.github.com>
2026-02-21 20:53:38 +0200

    Update 13_generate_citations.R

--- R/13_generate_citations.R
+++ R/13_generate_citations.R
@@ -2,44 +2,43 @@
 #          Automated Software Citation Generator           # 
 ############################################################
 
-generate_citations <- function() {
+generate_apa_citations <- function() {
   
-  # 1. Define the core software list
-  # We pull the names of all libraries currently loaded in the session
-  # that match the ones we initialized in init.R
   core_pkgs <- c("psych", "lavaan", "semPlot", "semTools", "MVN", 
                  "dplyr", "tibble", "interactions", "ggplot2", "patchwork", 
                  "gridExtra", "igraph", "purrr", "stringr", "tidySEM", 
                  "DiagrammeRsvg", "rsvg", "lavaanPlot", "ggplotify", "png", 
                  "grid", "cowplot")
   
-  # 2. Get R and RStudio info
-  software_names <- c("R", "RStudio")
-  software_versions <- c(
-    R.version.string,
-    if(exists("RStudio.Version")) paste0("RStudio ", RStudio.Version()$version) else "Not in RStudio"
-  )
+  cat("\n--- APA-Style Package Citations ---\n\n")
   
-  # 3. Loop through packages and grab versions automatically
+  # R itself
+  r_cit <- citation()
+  cat(paste0("R: ", r_cit$textVersion, "\n\n"))
+  
+  # RStudio
+  if (exists("RStudio.Version")) {
+    rs_cit <- paste0("RStudio: RStudio ", RStudio.Version()$version, "\n")
+    cat(rs_cit, "\n")
+  }
+  
+  # Loop through packages
   for (p in core_pkgs) {
     if (requireNamespace(p, quietly = TRUE)) {
-      software_names <- c(software_names, p)
-      software_versions <- c(software_versions, as.character(packageVersion(p)))
+      cit <- citation(p)
+      
+      # Some packages have multiple citations; usually take the first
+      if (length(cit) > 1) cit <- cit[[1]]
+      
+      # Print APA-like citation
+      cat(paste0(cit$textVersion, "\n\n"))
+    } else {
+      cat(paste0("Package '", p, "' not installed.\n\n"))
     }
   }
   
-  # 4. Create the final data frame
-  pkg_info <- data.frame(
-    Software = software_names,
-    Version = software_versions,
-    stringsAsFactors = FALSE
-  )
-  
-  # 5. Generate a simple text citation block for the manuscript
-  cat("\n--- Manuscript Citation Helper ---\n")
-  cat("All analyses were conducted in", R.version.string, "using the following packages:\n")
-  cat(paste(core_pkgs, collapse = ", "), ".\n")
-  cat("----------------------------------\n\n")
-  
-  return(pkg_info)
+  cat("--- End of Citations ---\n")
 }
+
+# Example usage
+pkg_citations <- generate_apa_citations()
